1c3eca331acb5fc84806e6d9d11d3509960cbdb7
Clodial <maravillamatthew@gmail.com>
2021-08-05 15:10:50 -0400

    pushing in some prefabs

---
+++ Assets/Src/LimbManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/**
+ * 
+ * Manages the connection between limbs, text for each limb and other information to be displayed and used when tapping on a limb.
+ * 
+ **/
+public class LimbManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Src/MainGame.cs
+++ Assets/Src/MainGame.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MainGame : MonoBehaviour
 {
+
+    public GameObject currentBody; //current 
+
     // Start is called before the first frame update
     void Start()
     {
