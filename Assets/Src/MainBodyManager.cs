ffd487efcd0e3c5d4c50edce9745efe87ca425bf
Clodial <maravillamatthew@gmail.com>
2021-08-06 02:11:19 -0400

    updated cameras for ARFoundation setup

---
+++ Assets/Src/MainBodyManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainBodyManager : MonoBehaviour
+{
+
+    Transform bodyTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bodyTransform = this.gameObject.transform;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
